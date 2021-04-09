af7592335d9c636c2e12e8e4ac86d359c6f0aa3c
Eugene Luo <eyluo@andrew.cmu.edu>
2021-04-08 19:26:20 -0700

    added optargs for depth and results

--- python/letterboxed.py
+++ python/letterboxed.py
@@ -5,10 +5,13 @@ import word_gen
 import word_graph
 
 NUM_ARGS = 5
-USAGE = 'Usage: ./letter_boxed.py <ltter set> <letter set> <letter set> <letter set>' +\
+NUM_OPTIONAL_ARGS = 2
+USAGE = 'Usage: ./letter_boxed.py <letter set> <letter set> <letter set> <letter set> <optional depth> <optional num results>' +\
         '\n\tEach <letter set> is a group of three letters like abc' +\
         '\n\tNo letter can be repeated within and across all four sets of letters'
-MAX_DEPTH = 3
+EXCEPTION_STR = 'Please specify integer values for optional arguments'
+DEFAULT_DEPTH = 2
+DEFAULT_NUM_RESULTS = 5
 
 
 def main():
@@ -18,7 +21,18 @@ def main():
 
     sides = sys.argv[1:NUM_ARGS]
 
-    # capitalize everything if not already capitalized
+    if len(sys.argv) >= NUM_ARGS + NUM_OPTIONAL_ARGS:
+        try:
+            depth = int(sys.argv[NUM_ARGS])
+            num_results = int(sys.argv[NUM_ARGS+1])
+        except:
+            print(EXCEPTION_STR)
+            return 1
+    else:
+        depth = DEFAULT_DEPTH
+        num_results = DEFAULT_NUM_RESULTS
+
+        # capitalize everything if not already capitalized
     sides = list(map(lambda side: list(map(lambda c: c.upper(), side)), sides))
 
     # get all the legal words
@@ -28,7 +42,9 @@ def main():
     graph = word_graph.Graph(word_set)
 
     # traverse the graph
-    print(graph.find_paths(sides, max_depth=MAX_DEPTH, max_results=-1))
+    print(graph.find_paths(sides, max_depth=depth, max_results=num_results))
+
+    return 0
 
 
 if __name__ == '__main__':
